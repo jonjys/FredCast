da1ea1f5fa1b58b1ace7fdb98b7486439c42b06f
FRED <fkornelind@hotmail.com>
2026-08-17 19:45:40 +0200

    v3: QrScanScreen + RootNavigator scan wiring

--- mobile/src/navigation/RootNavigator.tsx
+++ mobile/src/navigation/RootNavigator.tsx
@@ -11,6 +11,7 @@ import { DevicesScreen } from '../screens/DevicesScreen';
 import { SettingsScreen } from '../screens/SettingsScreen';
 import { NowPlayingScreen } from '../screens/NowPlayingScreen';
 import { QrConnectScreen } from '../screens/QrConnectScreen';
+import { QrScanScreen } from '../screens/QrScanScreen';
 import { LiveScreen } from '../screens/LiveScreen';
 import { useAutoConnectFromUrl } from '../cast/useAutoConnectFromUrl';
 import { AutoConnectBanner } from '../components/AutoConnectBanner';
@@ -30,6 +31,7 @@ export function RootNavigator() {
   const t = useTheme();
   const [nowPlayingOpen, setNowPlayingOpen] = useState(false);
   const [qrOpen, setQrOpen] = useState(false);
+  const [scanOpen, setScanOpen] = useState(false);
   const [connectToOpen, setConnectToOpen] = useState(false);
   const [liveOpen, setLiveOpen] = useState(false);
   const autoConnectStatus = useAutoConnectFromUrl();
@@ -68,7 +70,7 @@ export function RootNavigator() {
           <Tab.Screen name="Bibliotek">
             {() => <LibraryScreen onOpenNowPlaying={() => setNowPlayingOpen(true)} />}
           </Tab.Screen>
-          <Tab.Screen name="Skärmar">{() => <DevicesScreen onOpenQr={() => setQrOpen(true)} />}</Tab.Screen>
+          <Tab.Screen name="Skärmar">{() => <DevicesScreen onOpenQr={() => setQrOpen(true)} onOpenScan={() => setScanOpen(true)} />}</Tab.Screen>
           <Tab.Screen name="Inställningar" component={SettingsScreen} />
         </Tab.Navigator>
       </NavigationContainer>
@@ -85,6 +87,7 @@ export function RootNavigator() {
 
       <NowPlayingScreen visible={nowPlayingOpen} onClose={() => setNowPlayingOpen(false)} />
       <QrConnectScreen visible={qrOpen} onClose={() => setQrOpen(false)} />
+      <QrScanScreen visible={scanOpen} onClose={() => setScanOpen(false)} />
       <LiveScreen visible={liveOpen} onClose={() => setLiveOpen(false)} />
       <ConnectToSheet
         visible={connectToOpen}
